44ecca9f0111069004d78c2acf18872a297f0bbf
20jun01 <10junjun20@gmail.com>
2023-10-21 23:25:55 +0900

    🩹 fix type

--- bot/handler/cmd/mahjong.py
+++ bot/handler/cmd/mahjong.py
@@ -2,9 +2,12 @@ import random
 from mahjong import agari
 from mahjong.tile import TilesConverter
 
-def chinitsu_tehai_generator() -> list[str]:
+def chinitsu_tehai_generator() -> str:
     tiles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    return random.choices(tiles, k=13)
+    tiles_tmp = random.choices(tiles, k=13)
+    tiles_tmp.sort()
+    tiles_str = "".join(tiles_tmp)
+    return tiles_str
 
 def chinitsu_agari_check(tiles: str) -> bool:
     """
