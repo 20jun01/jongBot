b7d26c84008604a20054d30ced9b1ecf13961ac4
20jun01 <10junjun20@gmail.com>
2023-10-21 23:27:22 +0900

    🩹 fix message

--- bot/handler/model/tile.py
+++ bot/handler/model/tile.py
@@ -13,8 +13,8 @@ def convert_to_message(number: int, tile_type: TileType):
     if tile_type == TileType.Ji:
         if number < 1 or number > 7:
             raise ValueError('{} is not valid tile number.'.format(number))
-        return PREFIX + ji_list[number - 1]
-    return PREFIX + str(number) + tile_type.value
+        return ":" + PREFIX + ji_list[number - 1] + ":"
+    return ":" + PREFIX + str(number) + tile_type.value + ":"
 
 def convert_chinitsu_str_to_message(tiles: str, tile_type: TileType = TileType.Pin):
     message = ""
